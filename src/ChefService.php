06bd6d418471e0c3def407bcb60ab160a14f28d3
racinee <esteban.racineeecole@gmail.com>
2023-09-26 10:22:39 +0200

    classe ChefService

---
+++ src/ChefService.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+class ChefService extends Employe
+{
+    protected string $service;
+
+    public function __construct(string $prenom, string $nom, int $age, string $service)
+    {
+        parent::__construct($prenom, $nom, $age);
+        $this->service = $service;
+    }
+
+    /**
+     * @return string
+     */
+    public function getService(): string
+    {
+        return $this->service;
+    }
+
+
+
+    public function presentation(): string
+    {
+        return "Bonjour, je suis $this->prenom $this->nom, j'ai $this->age ans et je suis le chef du $this->service.";
+    }
+}
